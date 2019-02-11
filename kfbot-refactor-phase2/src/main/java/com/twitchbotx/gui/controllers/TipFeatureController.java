baf7d958202c754587a09b8ced3ff9efb4f99f5b
Raxa <raxastudios@gmail.com>
2019-02-10 19:48:04 -0800

    Starting progress on implementing tip feature

--- kfbot-refactor-phase2/src/main/java/com/twitchbotx/gui/controllers/TipFeatureController.java
+++ kfbot-refactor-phase2/src/main/java/com/twitchbotx/gui/controllers/TipFeatureController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.paint.Color;
 
 /**
  * FXML Controller class
@@ -24,19 +25,23 @@ public class TipFeatureController implements Initializable {
 
     ScreensController myController = new ScreensController();
     Datastore store;
-    
+
     @FXML
     ProgressBar pBar;
 
     @FXML
     ColorPicker bColor;
-    
+
     @FXML
     ColorPicker pColor;
-    
+
     @FXML
     ColorPicker tColor;
-    
+
+    //TODO: add ability to change incremential values
+    // ie per sub = 1 versus dollar value
+    //TODO: intialize all values from sql on open
+    // hex values, preview bar, enabled/disabled, text values
     /**
      * Initializes the controller class.
      */
@@ -44,12 +49,13 @@ public class TipFeatureController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         pBar.setProgress(0.50);
-        System.out.println("value of tColor: " + tColor.getValue() + " hashvalue: " + tColor.getValue().hashCode());
-    }    
-    
-    
-    
-    // TODO preview panel? 
+        pBar.setStyle("-fx-accent: blue");
+        bColor.setValue(Color.web("#ffffff"));
+        pColor.setValue(Color.BLUE);
+        tColor.setValue(Color.web("#ffccff"));
+        System.out.println("value of tColor: " + tColor.getValue().toString().substring(2, 8) + " hashvalue: " + tColor.getPromptText());
+    }
+
     @FXML
     public void dash() {
         setDimensions();
@@ -66,4 +72,50 @@ public class TipFeatureController implements Initializable {
         dm.setHeight(h);
         dm.setWidth(w);
     }
+
+    // color value setters
+    // need to reset all css values on each change
+    @FXML
+    public void setColors() {
+        //send to sql - _kfOverlay -> tipTracker -> bgColor
+
+        //update local bar
+        pBar.setStyle("-fx-background-color: #" + bColor.getValue().toString().substring(2, 8));
+        pBar.setStyle("-fx-color: #" + tColor.getValue().toString().substring(2, 8));
+        pBar.setStyle("-fx-accent: #" + pColor.getValue().toString().substring(2, 8));
+    }
+
+    //enabled disabled value setters
+    @FXML
+    public void submitBitEnabled() {
+
+    }
+
+    @FXML
+    public void submitSubEnabled() {
+
+    }
+
+    @FXML
+    public void submitTipEnabled() {
+
+    }
+
+    /**
+     * Capture and set all values values: heading(optional), text
+     * inside(optional), begin/total amount, description(optional)
+     */
+    @FXML
+    public void submitValues() {
+
+    }
+
+    /**
+     * Reset all local and sql values to defaults
+     */
+    @FXML
+    public void resetValues() {
+
+    }
+
 }
